faa9169b48c7b11526bfdcd87a7e2c09d71e6964
ruxandrafed <ruxandra.fediuc@gmail.com>
2016-11-06 19:13:14 -0800

    Update form to use angular form validation

--- gulpfile.js
+++ gulpfile.js
@@ -35,7 +35,7 @@ gulp.task('default', ['clean'], function() {
 });
 
 gulp.task('usemin',['jshint'], function () {
-  return gulp.src('./app/menu.html')
+  return gulp.src(['./app/menu.html', './app/contactus.html'])
     .pipe(usemin({
       css:[minifycss(),rev()],
       js: [ngannotate(),uglify(), rev()]
